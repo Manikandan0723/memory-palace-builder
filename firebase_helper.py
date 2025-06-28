76fc3577d1640e6a4b088052f5f84bbbe9f5f5f7
Manikandan0723 <149603365+Manikandan0723@users.noreply.github.com>
2025-06-28 20:42:06 +0000

    Fix Firebase config loading using Streamlit secrets

--- firebase_helper.py
+++ firebase_helper.py
@@ -1,10 +1,8 @@
 import pyrebase
 import json
-import os
 import streamlit as st
 
 def init_firebase():
-    # Get the config JSON string from secrets and parse it
     config = json.loads(st.secrets["FIREBASE_CONFIG"])
     firebase = pyrebase.initialize_app(config)
     return firebase
