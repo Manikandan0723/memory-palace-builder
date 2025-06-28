9455643fa21b9240e440d2bf0e9805359fd71920
Manikandan0723 <149603365+Manikandan0723@users.noreply.github.com>
2025-06-28 20:12:40 +0000

    Fix: updated firebase_helper.py to use st.secrets

--- firebase_helper.py
+++ firebase_helper.py
@@ -1,7 +1,16 @@
-import streamlit as st
+# firebase_helper.py
 import pyrebase
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def init_firebase():
-    config = dict(st.secrets["firebase"])
+    firebase_config_path = os.getenv("FIREBASE_CONFIG") or "firebase_config.json"
+
+    with open(firebase_config_path) as f:
+        config = json.load(f)
+
     firebase = pyrebase.initialize_app(config)
     return firebase
